57575b3aea1910de952840cfd25f05237e574c69
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-08-17 20:30:44 +0300

    Done.

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+def greet_programmer
+    p "Hello, programmer!"
+end 
+greet_programmer
+
+def greet(name)
+    p "Hello, #{name}!"
+end
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    p "Hello, #{name}!"
+end
+greet_with_default
+
+def add(num1,num2)
+    return num1+num2
+end
+ sum = add(1, 2)
+p "sum"
+
+def halve number
+    if number.class != Integer
+        return nil
+    end;
+
+    number / 2
+end
+puts halve "kllS" 
+  
+  
+  
